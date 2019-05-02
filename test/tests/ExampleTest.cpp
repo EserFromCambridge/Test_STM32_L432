52b44b25f4ddc9875c5ba4603f1ef544206d71e3
Eser Takmaz <etakmaz@asymptote.co.uk>
2019-05-02 16:28:42 +0100

    Add a small unittest example in order to try cpputest

---
+++ test/tests/ExampleTest.cpp
@@ -0,0 +1,46 @@
+/*
+ * TestExampleTest.cpp
+ *
+ *  Created on: 21 May 2018
+ *      Author: eser
+ */
+
+// CppuTest includes
+#include "CppUTest/TestHarness.h"
+#include "CppUTestExt/MockSupport.h"
+
+#include "unittest_ex.h"
+// Header for unit under test
+
+
+//Declare a test group
+TEST_GROUP(Example) {
+
+	// Declare a setup method.. Optional
+	void setup () {
+
+	}
+
+	// Declare a tearDown method.. Optional
+	void teardown () {
+
+	}
+};
+
+TEST(Example, Test1) {
+    /* Check two longs are equal */
+    LONGS_EQUAL(1, 1);
+}
+
+TEST(Example, Test2) {
+    /* Check a condition */
+    CHECK(true == true);
+
+    /* Check a string */
+    STRCMP_EQUAL("HelloWorld", "HelloWorld");
+}
+
+TEST(Example, example) {
+	LONGS_EQUAL(5, unittest_add(2, 3));
+}
+
